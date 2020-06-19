6d504698eaa4aab6609f87bd7940c23c1446abee
Abeer Bajpai <abeerdude@gmail.com>
2020-06-18 18:28:50 -0700

    smoothload.js

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -16,66 +16,6 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 	}, 1800);
 })
 
-document.addEventListener('click', function(e) {
-	var el = e.target;
-  
-	// Go up in the nodelist until we find a node with .href (HTMLAnchorElement)
-	while (el && !el.href) {
-	  el = el.parentNode;
-	}
-  
-	if (el) {
-	  e.preventDefault();
-	  history.pushState(null, null, el.href);
-	  changePage();
-	  return;
-	}
-  });
-
-  function loadPage(url) {
-	return fetch(url, {
-	  method: 'GET'
-	}).then(function(response) {
-	  return response.text();
-	});
-  }
-
-window.addEventListener('popstate', changePage);
-
-var main = document.querySelector('main');
-
-function changePage() {
-  // Note, the URL has already been changed
-  var url = window.location.href;
-
-  loadPage(url).then(function(responseText) {
-    var wrapper = document.createElement('div');
-        wrapper.innerHTML = responseText;
-
-    var oldContent = document.querySelector('.cc');
-    var newContent = wrapper.querySelector('.cc');
-
-    main.appendChild(newContent);
-    animate(oldContent, newContent);
-  });
-}
-
-function animate(oldContent, newContent) {
-	oldContent.style.position = 'absolute';
-  
-	var fadeOut = oldContent.animate({
-	  opacity: [1, 0]
-	}, 1000);
-  
-	var fadeIn = newContent.animate({
-	  opacity: [0, 1]
-	}, 1000);
-  
-	fadeIn.onfinish = function() {
-	  oldContent.parentNode.removeChild(oldContent);
-	};
-  }
-
 	$(function() {
 
 		var	$window = $(window),
