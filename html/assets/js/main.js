e9cd8e9528fb07940c71e714cd8f1804b019ec98
Abeer Bajpai <abeerdude@gmail.com>
2020-06-22 13:41:10 -0700

    Website is growing wooo

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -23,6 +23,18 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 	}, 500);
 })
 
+function myFunction(imgs) {
+	// Get the expanded image
+	var expandImg = document.getElementById("expandedImg");
+	// Get the image text
+	var imgText = document.getElementById("imgtext");
+	// Use the same src in the expanded image as the image being clicked on from the grid
+	expandImg.src = imgs.src;
+	// Use the value of the alt attribute of the clickable image as text inside the expanded image
+	imgText.innerHTML = imgs.alt;
+	// Show the container element (hidden with CSS)
+	expandImg.parentElement.style.display = "block";
+  }
 
 	$(function() {
 
