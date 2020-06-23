e25171cd5e663f0c9e5539e5f2c878302e6448c7
Abeer Bajpai <49898638+AbeerBPi@users.noreply.github.com>
2020-06-22 23:24:02 -0700

    Cooler fade transitions

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,3 +1,24 @@
+$(window).on("load",function() {
+	function fade(pageLoad) {
+	  var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+	  var min = 0.7;
+	  var max = 1;
+	  var threshold = 0.01;
+	  
+	  $(".fade").each(function() {
+		/* Check the location of each desired element */
+		var objectBottom = $(this).offset().top + $(this).outerHeight();
+		
+		/* If the element is completely within bounds of the window, fade it in */
+		if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+		  if ($(this).css("opacity")<=min+threshold || pageLoad) {$(this).fadeTo(500,max);}
+		} else { //object goes out of view (scrolling up)
+		  if ($(this).css("opacity")>=max-threshold || pageLoad) {$(this).fadeTo(500,min);}
+		}
+	  });
+	} fade(true); //fade elements on page-load
+	$(window).scroll(function(){fade(false);}); //fade elements on scroll
+  });
 (function($) {
 
 	skel.breakpoints({
