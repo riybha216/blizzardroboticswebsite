7ccba663e90012ebaf0fd08ed7c44175a84c7a62
Abeer Bajpai <abeerdude@gmail.com>
2020-07-02 23:04:29 -0700

    fade in

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,26 +1,10 @@
 // $('#myTurntable').turntable();
 
-$(window).on("load",function() {
-	function fade(pageLoad) {
-	  var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-	  var min = 0.6;
-	  var max = 1;
-	  var threshold = 0.01;
-	  
-	  $(".fade").each(function() {
-		/* Check the location of each desired element */
-		var objectBottom = $(this).offset().top + $(this).outerHeight();
-		
-		/* If the element is completely within bounds of the window, fade it in */
-		if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-		  if ($(this).css("opacity")<=min+threshold || pageLoad) {$(this).fadeTo(500,max);}
-		} else { //object goes out of view (scrolling up)
-		  if ($(this).css("opacity")>=max-threshold || pageLoad) {$(this).fadeTo(500,min);}
-		}
-	  });
-	} fade(true); //fade elements on page-load
-	$(window).scroll(function(){fade(false);}); //fade elements on scroll
-  });
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init(); // init the library
+
 (function($) {
 
 	skel.breakpoints({
