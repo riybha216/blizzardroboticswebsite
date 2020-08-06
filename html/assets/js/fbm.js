8467a48a500db99cfcda2e4190c80bce475f6c8d
Abeer Bajpai <abeerdude@gmail.com>
2020-08-06 11:55:15 -0700

    Update fbm.js

--- html/assets/js/fbm.js
+++ html/assets/js/fbm.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(){
-    window.fbAsyncInit = function () {
-        FB.init({
-            xfbml: true,
-            version: 'v8.0'
-        });
-    };
+// document.addEventListener("DOMContentLoaded", function(){
+//     window.fbAsyncInit = function () {
+//         FB.init({
+//             xfbml: true,
+//             version: 'v8.0'
+//         });
+//     };
 
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-});
+//     (function (d, s, id) {
+//         var js, fjs = d.getElementsByTagName(s)[0];
+//         if (d.getElementById(id)) return;
+//         js = d.createElement(s); js.id = id;
+//         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+//         fjs.parentNode.insertBefore(js, fjs);
+//     }(document, 'script', 'facebook-jssdk'));
+// });
